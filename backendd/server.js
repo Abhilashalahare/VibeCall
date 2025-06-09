5a5ddd8df22f55aec7866973a1050745de3b9980
Abhilasha Lahare <abhilashalahare6@gmail.com>
2025-06-09 15:36:23 +0530

    Moved Mongo URI to .env and updated server config

--- backendd/server.js
+++ backendd/server.js
@@ -9,13 +9,16 @@ import { connectToScoket } from './controllers/socketManager.js'
 import cors from 'cors'
 import router from './routes/users.routes.js'
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express()
 const server = createServer(app)
 // const io = new Server(server)  fro this make socketManager to manage it and pass the server variable
 const io = connectToScoket(server)
 
-app.set('port', (process.env.PORT || 8000))
+
 
 app.use(cors());
 app.use(express.json({limit:"40kb"}))
@@ -23,16 +26,16 @@ app.use(express.urlencoded({limit: "40kb", extended: true}))
 
 app.use("/api/v1/users", router)
 
-const start = async()=>{
-    const connectionDB = await mongoose.connect("mongodb+srv://abhilasha:himawari12@callix.gfzgjbc.mongodb.net/")
+const start = async () => {
+  const connectionDB = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`Mongo connected DB host: ${connectionDB.connection.host} `);
-    
-    server.listen(app.get('port'), ()=>{   //app.get('port) or 8000
-        console.log("Listening on Port", app.get('port'));
-        
-    })
-}
+  console.log(`Mongo connected DB host: ${connectionDB.connection.host}`);
+
+  server.listen(process.env.PORT, () => {
+    console.log("Listening on Port", process.env.PORT);
+  });
+};
+
 
 
 start()
